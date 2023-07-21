9c1181e10e9c6b93924be3ae41a620c8fc32a920
Kate Velichko <bboobb0503@gmail.com>
2023-07-21 21:30:03 +0300

    adding the main part of frontend

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './AboutMe.css';
+import foto from '../../images/avatar_img1.jpg';
+
+function AboutMe () {
+    return (
+        <section className='aboutMe' id='aboutMe'>
+            <h3 className='main__section-title'>Студент</h3>
+            <hr className="main__line"></hr>
+
+            <div className='aboutMe__content'>
+                <div className='aboutMe__textContent'>
+                    <h2 className='aboutMe__title'>Екатерина</h2>
+                    <p className='aboutMe__description'>Фронтенд-разработчик, 27 лет</p>
+
+                    <p className='aboutMe__text'>Выросла, живу и работаю в Москве. Окончила бакалавриат и магистратуру Дипакадемии МИД РФ. С программированием никак до покупки курса связана не была. Это для меня совершенно новый интересный мир, который приходится познавать с нуля. Разве что можно упомянуть несколько бесплатных курсов на популярных программистких платформах. Все эти трудности мне по душе. В будущем планирую и дальше развиваться в этой сфере и изучать другие языки программирования.  </p>
+                    <a href="https://github.com/Evelichko" target="_blank" className='aboutMe__link' rel="noreferrer">Github</a>
+                </div>
+                
+                <img  className='aboutMe__foto' src={foto} alt="фото"/>
+                
+            </div>
+
+        </section>
+    )
+}
+
+export default AboutMe;
